a2e0d7ed76d1a97d1d47525fef30e2015b2f439f
Timak304 <david.fonquernie@gmail.com>
2017-11-21 19:46:47 +0100

    Initial commit

---
+++ Assets/movment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour {
+
+	public float acceleration = 0.3f;
+	public float braking = 0.3f;
+	public float steering = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// update for physics
+	void FixedUpdate() {
+		// steering
+		float lSteering = 0;
+		float lAcceleration = 0;
+		if (Input.GetKey (KeyCode.UpArrow)) lAcceleration++;
+		if (Input.GetKey (KeyCode.DownArrow)) lAcceleration--;
+		if (Input.GetKey (KeyCode.RightArrow)) lSteering++;
+		if (Input.GetKey (KeyCode.LeftArrow)) lSteering--;
+
+		GetComponent<Rigidbody2D>().AddTorque(lSteering * steering * -0.2f);
+
+		// apply car movement
+		GetComponent<Rigidbody2D>().AddForce(transform.right * lAcceleration * acceleration * 50.0f);
+
+	}
+}
